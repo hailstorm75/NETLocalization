f301112afc568aa6aaf9c89aa046befd7d477350
Denis Akopyan <denis.akopyan@seznam.cz>
2025-07-02 08:48:49 +0200

    #3 - Introduce the markup extension

---
+++ src/Framework/Localization.Avalonia/TrArgumentConverter.cs
@@ -0,0 +1,62 @@
+﻿using Localization.Shared.Models;
+using Avalonia.Data.Converters;
+using Avalonia.Data;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System;
+
+namespace Localization.Avalonia;
+
+internal sealed class TrArgumentConverter : IMultiValueConverter
+{
+    public required LString Data { get; init; }
+    public required IReadOnlyCollection<IBinding> Arguments { get; init; } = [];
+    public required IValueConverter? Converter { get; init; }
+    public required object? ConverterParameter { get; init; }
+    public required CultureInfo ConverterCulture { get; init; }
+
+    /// <inheritdoc />
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var localizationArguments = new List<object>();
+        var offset = 1; // The first value is always the localized string
+
+        foreach (var argument in Arguments)
+        {
+            if (argument is MultiBinding multiBindingArg)
+            {
+                var bindingsCount = multiBindingArg.Bindings.Count;
+                // You may need to store the converter instance for each argument if you use nested MultiBindings
+                if (multiBindingArg.Converter is not null)
+                {
+                    var arg = multiBindingArg.Converter.Convert(
+                        values.Skip(offset).Take(bindingsCount).ToList(),
+                        targetType,
+                        multiBindingArg.ConverterParameter,
+                        multiBindingArg.ConverterCulture ?? culture
+                    );
+                    localizationArguments.Add(arg ?? string.Empty);
+                }
+                offset += bindingsCount;
+            }
+            else
+            {
+                if (values.Count > offset)
+                    localizationArguments.Add(values[offset] ?? string.Empty);
+                offset++;
+            }
+
+        }
+
+        var translated = string.Format(Data.String, localizationArguments.ToArray());
+
+        if (Converter is not null)
+            return Converter.Convert(translated, targetType, ConverterParameter, ConverterCulture ?? culture)?.ToString() ?? string.Empty;
+
+        return translated;
+    }
+
+    public object[] ConvertBack(object? value, Type[] targetTypes, object? parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
---
+++ src/Framework/Localization.Avalonia/TrExtension.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Markup.Xaml;
+using Avalonia.Data.Converters;
+using Localization.Shared.Models;
+
+namespace Localization.Avalonia;
+
+/// <summary>
+/// Localization markup extension for Avalonia
+/// </summary>
+public sealed class TrExtension : MarkupExtension
+{
+    private readonly IBinding[] _args = [];
+
+    #region Properties
+
+    public string Key { get; set; } = string.Empty;
+    public string Namespace { get; set; } = string.Empty;
+    public LString? String { get; set; }
+    public IValueConverter? Converter { get; set; }
+    public object? ConverterParameter { get; set; }
+    public CultureInfo? ConverterCulture { get; set; }
+
+    #endregion
+
+    #region Constructors
+
+    private static IBinding SanitizeArgument(object argument)
+        => argument switch
+        {
+            IBinding binding => binding,
+            LString locString => new Binding { Path = nameof(LString.String), Source = locString },
+            _ => new Binding { Source = argument }
+        };
+
+    public TrExtension() { }
+
+    public TrExtension(object arg0)
+    {
+        _args = new[] { SanitizeArgument(arg0) };
+    }
+
+    public TrExtension(IBinding arg0, IBinding arg1)
+    {
+        _args = new[] { SanitizeArgument(arg0), SanitizeArgument(arg1) };
+    }
+
+    public TrExtension(IBinding arg0, IBinding arg1, IBinding arg2)
+    {
+        _args = new[] { SanitizeArgument(arg0), SanitizeArgument(arg1), SanitizeArgument(arg2) };
+    }
+
+    public TrExtension(IBinding arg0, IBinding arg1, IBinding arg2, IBinding arg3)
+    {
+        _args = new[] { SanitizeArgument(arg0), SanitizeArgument(arg1), SanitizeArgument(arg2), SanitizeArgument(arg3) };
+    }
+
+    public TrExtension(IBinding arg0, IBinding arg1, IBinding arg2, IBinding arg3, IBinding arg4)
+    {
+        _args = new[] { SanitizeArgument(arg0), SanitizeArgument(arg1), SanitizeArgument(arg2), SanitizeArgument(arg3), SanitizeArgument(arg4) };
+    }
+
+    #endregion
+
+    /// <inheritdoc />
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        var localizedString = GetLocString(Namespace, Key, String);
+        if (localizedString is null)
+            return string.Empty;
+
+        if (_args.Length == 0)
+            return BindingWithoutArguments(localizedString);
+
+        return BindingWithArguments(localizedString);
+    }
+
+    private static LString? GetLocString(string @namespace, string key, LString? locString)
+    {
+        if (locString is not null)
+            return locString;
+
+        if (string.IsNullOrWhiteSpace(@namespace) || string.IsNullOrWhiteSpace(key))
+            return null;
+
+        return new LString
+        {
+            Namespace = @namespace,
+            Key = key
+        };
+    }
+
+    private object BindingWithArguments(LString localizedString)
+    {
+        // You must implement TrArgumentConverter for Avalonia (similar to WPF's IMultiValueConverter)
+        var converter = new TrArgumentConverter
+        {
+            Arguments = _args,
+            Data = localizedString,
+            Converter = Converter,
+            ConverterParameter = ConverterParameter,
+            ConverterCulture = ConverterCulture ?? CultureInfo.CurrentCulture
+        };
+
+        var multiBinding = new MultiBinding
+        {
+            Converter = converter,
+            Bindings =
+            {
+                new Binding { Path = nameof(LString.String), Source = localizedString }
+            }
+        };
+
+        foreach (var arg in _args)
+            multiBinding.Bindings.Add(arg);
+
+        return multiBinding;
+    }
+
+    private static object BindingWithoutArguments(LString localizedString)
+        => new Binding
+        {
+            Path = nameof(LString.String),
+            Source = localizedString
+        };
+}
