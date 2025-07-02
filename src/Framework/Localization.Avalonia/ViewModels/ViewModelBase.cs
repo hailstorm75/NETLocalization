4f90cfee7e3652306e2072da77d72c5f167d50cf
Denis Akopyan <denis.akopyan@seznam.cz>
2025-07-02 08:48:32 +0200

    #3 - Remove unnecessary application code

--- src/Framework/Localization.Avalonia/ViewModels/ViewModelBase.cs
+++
@@ -1,9 +0,0 @@
-using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Localization.Avalonia.ViewModels;
-public class ViewModelBase : ReactiveObject
-{
-}
