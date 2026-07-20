a12b5dff4fb6614de4055a24db1eb19868ea6084
Denis Akopjan <denis.akopyan@seznam.cz>
2026-07-20 21:20:02 +0200

    #1 - Add explanatory comments to example code

--- examples/Example.WPF/Status.cs
+++ examples/Example.WPF/Status.cs
@@ -2,14 +2,17 @@ using Localization.Shared.Attributes;
 
 namespace Example.WPF;
 
-[LocalizedEnum<Provider>]
+[LocalizedEnum<Provider>] // Required decoration to make an enum localizable
 public enum Status
 {
+    // Localization retrieved automatically from parent `LocalizedEnum<Provider>` attribute
     Draft,
 
+    // Specific localization from type-safe field
     [LocalizedEnumField(nameof(Provider.PublishedStatus))]
     Published,
 
+    // Another way to specify the specific localization using type-safe field access
     [LocalizedEnumField(typeof(Provider), nameof(Provider.Archived))]
     Archived
 }
